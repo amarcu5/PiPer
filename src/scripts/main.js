330df7d41ff619d55855315edce86da9f53702cc
Thibault Hendrieckx <thibault.hendrieckx@vub.be>
2018-05-31 12:50:02 +0200

    Supports YeloTV

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -874,6 +874,34 @@ const resources = {
     },
   },
 
+  'yeloplay': {
+    buttonClassName: 'button',
+    buttonDidAppear: function() {
+      const parent = document.getElementById("PiPer_0");
+      parent.style.width = "210px";
+      bypassBackgroundTimerThrottling();
+    },
+    buttonInsertBefore: function(/** Element */ parent) {
+      return document.getElementsByTagName("player-fullscreen-button")[0];
+    },
+    buttonParent: function() {
+      return document.getElementsByClassName("buttons")[0];
+    },
+    buttonScale: 0.8,
+    buttonStyle: /** CSS */ (`
+      margin-bottom: -10px;
+      margin-left: 10px;
+      width: 50px;
+      cursor: pointer;
+      opacity: 0.8;
+      height: 40px !important;
+      margin-bottom: 0px !important;
+    `),
+    videoElement: function() {
+      return document.querySelector("video[src]");
+    },
+  },
+
   'youtube': {
     buttonClassName: 'ytp-button',
     buttonDidAppear: function() {
