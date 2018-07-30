a021a402595dc03b914917cb2ac6d403a659dc86
amarcu5 <adampetermarcus@gmail.com>
2018-07-30 22:15:32 +0100

    Fixed YouTube buffering bug
    Bypass of background timer throttling now achieved by calling all timeouts with media 'timeupdate' events and additionally clearTimeout has been implemented

--- src/scripts/fix.js
+++ src/scripts/fix.js
@@ -3,13 +3,14 @@
 
   let activeVideo = null;
   let timeoutId = 0;
+  let timeouts = {};
 
   const requests = [];
   const callbacks = [];
-  const timeouts = [];
 
-  const originalRequestAnimationFrame = window.requestAnimationFrame;
   const originalSetTimeout = window.setTimeout;
+  const originalClearTimeout = window.clearTimeout;
+  const originalRequestAnimationFrame = window.requestAnimationFrame;
 
   /**
    * Tracks animation frame requests and forwards requests when page visible
@@ -17,7 +18,7 @@
   const trackAnimationFrameRequest = function(callback) {
     let request = 0;
 
-    if (!document.hidden) {
+    if (!activeVideo) {
       request = originalRequestAnimationFrame(callback);
       requests.push(request);
     }
@@ -43,41 +44,63 @@
    * Calls tracked animation frame requests and timeouts
    */
   const callAnimationFrameRequestsAndTimeouts = function() {
+    
+    // Copy animation frame callbacks before calling to prevent endless looping
     const callbacksCopy = callbacks.slice();
     callbacks.length = 0;
 
+    // Call animation frame requests
     const timestamp = window.performance.now();
     for (let callback; callback = callbacksCopy.pop();) {
       callback(timestamp);
-    };
+    }
 
-    const timeoutsCopy = timeouts.slice();
-    timeouts.length = 0;
-
-    for (let timeout; timeout = timeoutsCopy.pop();) {
-      timeout();
-    };
+    // Copy timeouts to prevent endless looping
+    const timeoutsCopy = timeouts;
+    timeouts = {};
+    
+    // Call elapsed timeouts
+    for (let id in timeoutsCopy) {
+      let timeout = timeoutsCopy[id];
+      if (timeout[0] <= timestamp) {
+        if (typeof timeout[1] == "function") {
+          timeout[1]();
+        } else {
+          eval(/** @type {string} */ (timeout[1]));
+        }
+      } else {
+        timeouts[id] = timeout;
+      }
+    }
   };
 
   /**
-   * Avoids background throttling by invoking small timeouts with media 'timeupdate' events
+   * Avoids background throttling by invoking timeouts with media 'timeupdate' events
    *
    * @param {Function|string} callback
    * @param {number=} timeout
    * @return {number}
    */
   const unthrottledSetTimeout = function(callback, timeout) {
-    if (timeout >= 500) return originalSetTimeout(callback, timeout);
+    const id = timeoutId++;
+    timeouts[id] = [window.performance.now() + (timeout || 0), callback];
+    return id;
+  };
 
-    timeouts.push(callback);
-
-    return timeoutId++;
+  /**
+   * Clears queued timeouts to be invoked with media 'timeupdate' events
+   *
+   * @param {number} id
+   */
+  const unthrottledClearTimeout = function(id) {
+    delete timeouts[id];
   };
 
   /**
    * Bypasses background timer throttling when video playing picture in picture
    */
   const bypassBackgroundTimerThrottling = function() {
+
     if (document.hidden) {
 
       const allVideos = document.querySelectorAll('video');
@@ -95,23 +118,30 @@
       }
 
       window.setTimeout = unthrottledSetTimeout;
+      window.clearTimeout = unthrottledClearTimeout;
+      
       activeVideo.addEventListener('timeupdate', callAnimationFrameRequestsAndTimeouts);
 
     } else if (activeVideo) {
+
+      activeVideo = null;
+
+      window.setTimeout = originalSetTimeout;
+      window.clearTimeout = originalClearTimeout;
+
+      activeVideo.removeEventListener('timeupdate', callAnimationFrameRequestsAndTimeouts);
 
       for (let callbackId = callbacks.length; callbackId--;) {
         let request = originalRequestAnimationFrame(callbacks[callbackId]);
         requests.push(request);
       }
 
-      window.setTimeout = originalSetTimeout;
-      activeVideo.removeEventListener('timeupdate', callAnimationFrameRequestsAndTimeouts);
-
-      for (let timeout; timeout = timeouts.pop();) {
-        timeout();
-      };
-
-      activeVideo = null;
+      const timestamp = window.performance.now();
+      for (let id in timeouts) {
+        let timeout = timeouts[id];
+        originalSetTimeout(timeout[1], timeout[0] - timestamp);
+      }
+      timeouts = {};
     }
   };
   document.addEventListener('visibilitychange', bypassBackgroundTimerThrottling);
