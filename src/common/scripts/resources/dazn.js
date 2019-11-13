caaeeb01d98359aa945862a13794602dc37177de
amarcu5 <adampetermarcus@gmail.com>
2019-11-13 20:01:44 +0000

    Updated readme
    Added DAZN support

--- src/common/scripts/resources/dazn.js
+++ src/common/scripts/resources/dazn.js
@@ -2,29 +2,29 @@ export const domain = 'dazn';
 
 export const resource = {
   buttonStyle: (`
-	width: 1.5rem;
-	height: 1.5rem;
-	color: white;
-	background: transparent;
-	position: relative;
-	border: none;
-	outline: none;
-	border-radius: 0;
-	cursor: pointer;
-	-webkit-appearance: none;
-	margin: 0.5rem;
-	z-index: 1;
+    width: 1.5rem;
+    height: 1.5rem;
+    color: white;
+    background: transparent;
+    position: relative;
+    border: none;
+    outline: none;
+    border-radius: 0;
+    cursor: pointer;
+    -webkit-appearance: none;
+    margin: 0.5rem;
+    z-index: 1;
   `),
-  buttonInsertBefore: function(parent) {
-	// The Live indicator might move/cover the PiP button, just place the PiP button before it
-	var liveIndicator = document.querySelector('div[data-test-id^="PLAYER_LIVE_INDICATOR"]');
-	if (liveIndicator) {
-		return liveIndicator;
-	}
+  buttonInsertBefore: function(/** Element */ parent) {
+  	// The Live indicator might move/cover the PiP button, just place the PiP button before it
+  	const liveIndicator = document.querySelector('div[data-test-id^="PLAYER_LIVE_INDICATOR"]');
+  	if (liveIndicator) {
+      return liveIndicator;
+  	}
     return parent.lastChild;
   },
   buttonParent: function() {
-	return document.querySelector('div[data-test-id^="PLAYER_BAR"]');	
+    return document.querySelector('div[data-test-id^="PLAYER_BAR"]');	
   },
   videoElement: function() {
     return document.querySelector('div[data-test-id^="PLAYER_SOLUTION"] video');
