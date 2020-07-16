f3ace9cc3924cfa50a5905427e32c261ef1b2a27
aONe <aonez@users.noreply.github.com>
2020-07-16 10:48:06 +0200

    MotoGP support

---
+++ src/common/scripts/motogp.js
@@ -0,0 +1,33 @@
+export const domain = 'motogp';
+
+export const resource = {
+  buttonStyle: (`
+    width: 3em;
+    height: 3em;
+    color: white;
+    background: transparent;
+    position: relative;
+    border: none;
+    outline: none;
+    border-radius: 0;
+    cursor: pointer;
+    -webkit-appearance: none;
+    z-index: 1;
+  `),
+  buttonHoverStyle: (`
+    background: radial-gradient(rgba(255, 255, 255, 0.2), rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.05), rgba(255, 255, 255, 0.01), rgba(255, 255, 255, 0)) !important;
+  `),
+  buttonInsertBefore: function(parent) {
+	const fullScreenButton = document.querySelector('.vjs-fullscreen-control');
+  	if (fullScreenButton) {
+      return fullScreenButton;
+  	}
+    return parent.lastChild;
+  },
+  buttonParent: function() {
+    return document.querySelector('.vjs-control-bar');	
+  },
+  videoElement: function() {
+    return document.querySelector('video[class="vjs-tech"]');
+  }
+};
