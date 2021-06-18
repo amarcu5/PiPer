bf73a021e2fa6296a98e108b1b755371f9b535cf
aone <aone.jga@gmail.com>
2021-06-18 10:29:51 +0200

    Fix for Motogp script

--- src/common/scripts/resources/motogp.js
+++ src/common/scripts/resources/motogp.js
@@ -17,11 +17,11 @@ export const resource = {
   buttonHoverStyle: (`
     background: radial-gradient(rgba(255, 255, 255, 0.2), rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.05), rgba(255, 255, 255, 0.01), rgba(255, 255, 255, 0)) !important;
   `),
-  buttonInsertBefore: function(parent) {
-	const fullScreenButton = document.querySelector('.vjs-fullscreen-control');
-  	if (fullScreenButton) {
+  buttonInsertBefore: function(/** Element */ parent) {
+    const fullScreenButton = document.querySelector('.vjs-fullscreen-control');
+    if (fullScreenButton) {
       return fullScreenButton;
-  	}
+    }
     return parent.lastChild;
   },
   buttonParent: function() {
